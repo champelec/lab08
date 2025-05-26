c96f7921efc438c4d130af614d75e4298f1860e8
Shamshetdinov <@bk.ru>
2025-05-26 16:24:25 +0300

    finish

---
+++ build/CMakeFiles/lab06.dir/link.d
@@ -0,0 +1,28 @@
+lab06: \
+  /usr/lib/gcc/aarch64-linux-gnu/14/../../../aarch64-linux-gnu/libm.so \
+  /usr/lib/gcc/aarch64-linux-gnu/14/../../../aarch64-linux-gnu/libm.so \
+  /usr/lib/gcc/aarch64-linux-gnu/14/libgcc_s.so \
+  /usr/lib/gcc/aarch64-linux-gnu/14/libgcc_s.so \
+  /usr/lib/gcc/aarch64-linux-gnu/14/../../../aarch64-linux-gnu/libc.so \
+  /usr/lib/gcc/aarch64-linux-gnu/14/../../../aarch64-linux-gnu/libc.so \
+  /usr/lib/gcc/aarch64-linux-gnu/14/libgcc_s.so \
+  /usr/lib/gcc/aarch64-linux-gnu/14/libgcc_s.so \
+  /lib/ld-linux-aarch64.so.1
+
+/usr/lib/gcc/aarch64-linux-gnu/14/../../../aarch64-linux-gnu/libm.so:
+
+/usr/lib/gcc/aarch64-linux-gnu/14/../../../aarch64-linux-gnu/libm.so:
+
+/usr/lib/gcc/aarch64-linux-gnu/14/libgcc_s.so:
+
+/usr/lib/gcc/aarch64-linux-gnu/14/libgcc_s.so:
+
+/usr/lib/gcc/aarch64-linux-gnu/14/../../../aarch64-linux-gnu/libc.so:
+
+/usr/lib/gcc/aarch64-linux-gnu/14/../../../aarch64-linux-gnu/libc.so:
+
+/usr/lib/gcc/aarch64-linux-gnu/14/libgcc_s.so:
+
+/usr/lib/gcc/aarch64-linux-gnu/14/libgcc_s.so:
+
+/lib/ld-linux-aarch64.so.1:
